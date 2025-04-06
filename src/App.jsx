94b2e811c1b06e7bd16bed234122d2274cfcf144
KeshavS05 <KeshavS05>
2025-04-06 16:36:14 -0500

    Adds Modal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Modal from './modal';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div>
       <h1>React Starter Code for Inclusive Tech Competition!</h1>
       <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          This button increases count; count is currently {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and <code>src/App.css</code> to get started!
-        </p>
+        <h1>Modal Example</h1>
+        <button onClick={() => setIsModalOpen(true)}>Open Modal</button>
+        {isModalOpen && (
+          <Modal onClose={() => setIsModalOpen(false)} title="Modal Title" description={"This is a description"}/>
+        )}
       </div>
     </div>
   )
