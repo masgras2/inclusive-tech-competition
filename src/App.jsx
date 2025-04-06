f552f5f6b74b612395ddd09b7a93b7ce3a786fc5
masgras2 <masgras2@illinois.edu>
2025-04-06 16:34:06 -0500

    tailwind start

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [opportunities, setOpportunities] = useState(0)
   const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>React Starter Code for Inclusive Tech Competition!</h1>
       <div>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-red-500 text-4xl font-4xl" onClick={() => setCount((count) => count + 1)}>
           This button increases count; count is currently {count}
         </button>
         <p>
